63b06dab6bb0dde88d7a48d88fc5f32f4cb6957d
Edigleysson Silva <edigleyssonsilva@gmail.com>
2022-05-07 00:24:15 -0300

    add project to study tests

---
+++ automated_tests/lib/automated_tests.ex
@@ -0,0 +1,18 @@
+defmodule AutomatedTests do
+  @moduledoc """
+  Documentation for `AutomatedTests`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AutomatedTests.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
